4274e261820bddb0c0c9d91259b674ff2cadb4e3
Erik <fderpt@protonmail.com>
2021-09-28 14:29:52 +0200

    Added Philosophy section

--- app.js
+++ app.js
@@ -25,3 +25,72 @@ var myScrollFunc = function() {
   };
   
   window.addEventListener("scroll", myScrollFunc);
+
+  const link1 = document.getElementById('link1')
+  const link2 = document.getElementById('link2')
+  const link3 = document.getElementById('link3')
+  const text1 = document.getElementById('text1')
+  const text2 = document.getElementById('text2')
+  const text3 = document.getElementById('text3')
+
+  const textLinks = document.getElementById('texts-links')
+
+  textLinks.addEventListener("click", e => {     
+
+      if(e.target.id === 'link1') {
+        link1.classList.add("active");
+        link2.classList.remove("active");
+        link3.classList.remove("active");   
+        text1.style.display = 'block';
+        text2.style.display = 'none';
+        text3.style.display = 'none';
+      }
+
+      if(e.target.id === 'link2') {
+        link1.classList.remove("active");
+    link2.classList.add("active");
+    link3.classList.remove("active");   
+    text1.style.display = 'none';
+    text2.style.display = 'block';
+    text3.style.display = 'none';
+      }
+
+      if(e.target.id === 'link3') {
+        link1.classList.remove("active");
+    link2.classList.remove("active");
+    link3.classList.add("active");   
+    text1.style.display = 'none';
+    text2.style.display = 'none';
+    text3.style.display = 'block';
+      }
+  })
+
+//   text2.style.display = 'none';
+//   text3.style.display = 'none';
+
+//   link1.addEventListener("click", () => {
+//       link1.classList.add("active");
+//       link2.classList.remove("active");
+//       link3.classList.remove("active");   
+//       text1.style.display = 'block';
+//       text2.style.display = 'none';
+//       text3.style.display = 'none';
+//   })
+
+//   link2.addEventListener("click", () => {
+//     link1.classList.remove("active");
+//     link2.classList.add("active");
+//     link3.classList.remove("active");   
+//     text1.style.display = 'none';
+//     text2.style.display = 'block';
+//     text3.style.display = 'none';
+// })
+
+// link3.addEventListener("click", () => {
+//     link1.classList.remove("active");
+//     link2.classList.remove("active");
+//     link3.classList.add("active");   
+//     text1.style.display = 'none';
+//     text2.style.display = 'none';
+//     text3.style.display = 'block';
+// })
